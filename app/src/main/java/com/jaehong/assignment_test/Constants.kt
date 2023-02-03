3e86771ec8274d4a2ebba0f540ee3f98abc98c6b
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-02-03 21:05:35 +0900

    feat: NetworkModule 구현

---
+++ app/src/main/java/com/jaehong/assignment_test/Constants.kt
@@ -0,0 +1,9 @@
+package com.jaehong.assignment_test
+
+object Constants {
+    const val BASE_URL = "https://openapi.naver.com/v1/search/"
+    const val CLIENT_ID_KEY = "X-Naver-Client-Id"
+    const val CLIENT_SECRET_KEY = "X-Naver-Client-Secret"
+    const val CLIENT_ID_VALUE = "2pOgNlU72LFw_uyOyj63"
+    const val CLIENT_SECRET_VALUE = "yNKgp1resk"
+}
