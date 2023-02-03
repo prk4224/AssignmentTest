761ef9a720b33196c3c2c4430b681c3ea993e578
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-02-03 21:09:46 +0900

    feat: Api 호출 후 받아올 data class 구현

---
+++ domain/src/main/java/com/jaehong/domain/model/MovieItem.kt
@@ -0,0 +1,9 @@
+package com.jaehong.domain.model
+
+data class MovieItem(
+    val title: String,
+    val link: String,
+    val image: String,
+    val pubDate: String,
+    val userRating: String
+)
---
+++ domain/src/main/java/com/jaehong/domain/model/MovieItems.kt
@@ -0,0 +1,5 @@
+package com.jaehong.domain.model
+
+data class MovieItems(
+    val items: List<MovieItem>
+)
