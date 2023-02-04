180f73d1c5ad9a5a0d808c922368f8292b2b8066
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-02-04 17:13:53 +0900

    feat: MainActivity 에 HoemScreen 추가

--- presenter/src/main/java/com/jaehong/presentation/MainActivity.kt
+++ presenter/src/main/java/com/jaehong/presentation/MainActivity.kt
@@ -10,33 +10,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.jaehong.presentation.ui.screen.home.HomeScreen
+import com.jaehong.presentation.ui.screen.search.SearchScreen
+import com.jaehong.presentation.ui.theme.AssignmentTestTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            com.jaehong.presentation.ui.theme.AssignmentTestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+           HomeScreen()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    com.jaehong.presentation.ui.theme.AssignmentTestTheme {
-        Greeting("Android")
     }
 }
