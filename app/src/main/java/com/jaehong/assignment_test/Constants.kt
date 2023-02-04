960ec7e5d8c15b4cd0b6253a1d2a064ce0bacca3
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-02-04 14:36:03 +0900

    refactor: key 변경

--- app/src/main/java/com/jaehong/assignment_test/Constants.kt
+++ app/src/main/java/com/jaehong/assignment_test/Constants.kt
@@ -4,6 +4,6 @@ object Constants {
     const val BASE_URL = "https://openapi.naver.com/v1/search/"
     const val CLIENT_ID_KEY = "X-Naver-Client-Id"
     const val CLIENT_SECRET_KEY = "X-Naver-Client-Secret"
-    const val CLIENT_ID_VALUE = "2pOgNlU72LFw_uyOyj63"
-    const val CLIENT_SECRET_VALUE = "yNKgp1resk"
+    const val CLIENT_ID_VALUE = "lbC6jcsQ7ikYCxEJh8d9"
+    const val CLIENT_SECRET_VALUE = "CyAHBA_tu0"
 }
