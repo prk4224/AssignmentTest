48a4afdaf094cf5d463dab783efc8533417835d9
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-02-04 17:15:28 +0900

    faet: 목록 화면 구현

---
+++ presenter/src/main/java/com/jaehong/presentation/ui/screen/search/info/SearchInfoItems.kt
@@ -0,0 +1,26 @@
+package com.jaehong.presentation.ui.screen.search.info
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.jaehong.domain.model.MovieItem
+
+@Composable
+fun SearchInfoItems(
+    list: List<MovieItem>,
+    modifier: Modifier,
+    headerItemScreen: @Composable () -> Unit,
+    itemScreen: @Composable (MovieItem) -> Unit,
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        item {
+            headerItemScreen()
+        }
+        items(list) {
+            itemScreen(it)
+        }
+    }
+}
