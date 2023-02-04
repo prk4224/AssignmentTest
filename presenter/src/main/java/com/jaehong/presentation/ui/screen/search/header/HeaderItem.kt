01e3fcbd0b9f02309ab04ffc081f4555f9a24a7a
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-02-04 17:14:21 +0900

    feat: 검색 부분을 담당할 Header 구현

---
+++ presenter/src/main/java/com/jaehong/presentation/ui/screen/search/header/HeaderItem.kt
@@ -0,0 +1,64 @@
+package com.jaehong.presentation.ui.screen.search.header
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HeaderItem(
+    modifier: Modifier,
+    text: String,
+    setText: (String) -> Unit,
+    searchOnClicked: () -> Unit,
+    recentOnClicked: () -> Unit
+) {
+
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+       TextField(
+           value = text,
+           onValueChange = setText,
+           maxLines = 1,
+
+           modifier = Modifier
+               .weight(4f)
+       )
+
+        Spacer(modifier = Modifier.size(5.dp))
+        
+        Button(
+            onClick = { searchOnClicked() },
+            colors = ButtonDefaults.buttonColors(Color.LightGray),
+            contentPadding = PaddingValues(),
+            shape = RoundedCornerShape(10),
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1.5f)
+
+        ) {
+            Text(text = "검색", color = Color.Black)
+        }
+        Spacer(modifier = Modifier.size(5.dp))
+
+        Button(
+            onClick = { recentOnClicked() },
+            colors = ButtonDefaults.buttonColors(Color.LightGray),
+            contentPadding = PaddingValues(),
+            shape = RoundedCornerShape(10),
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1.5f)
+        ) {
+            Text(text = "최근 검색", color = Color.Black)
+        }
+    }
+}
