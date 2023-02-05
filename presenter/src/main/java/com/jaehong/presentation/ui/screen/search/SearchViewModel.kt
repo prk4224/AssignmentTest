1c7be33dfb0fa383eeacfaf3207a45471c02159b
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-02-05 13:20:32 +0900

    feat: insert 함수 추가

--- presenter/src/main/java/com/jaehong/presentation/ui/screen/search/SearchViewModel.kt
+++ presenter/src/main/java/com/jaehong/presentation/ui/screen/search/SearchViewModel.kt
@@ -1,13 +1,15 @@
 package com.jaehong.presentation.ui.screen.search
 
-import android.accounts.NetworkErrorException
+import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.jaehong.domain.model.ApiResult
+import com.bumptech.glide.Glide.init
 import com.jaehong.domain.model.MovieItem
 import com.jaehong.domain.usecase.GetSearchInfoUseCase
 import com.jaehong.presentation.navigation.Destination
 import com.jaehong.presentation.navigation.SearchAppNavigator
+import com.jaehong.presentation.util.checkedResult
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -51,6 +53,23 @@ class SearchViewModel @Inject constructor(
                     URLEncoder.encode(link, "UTF-8")
                 }
             searchAppNavigator.navigateTo(Destination.WebView(encodedUrl))
+        }
+    }
+
+    fun onNavigateToRecentClicked() {
+        viewModelScope.launch {
+            searchAppNavigator.navigateTo(Destination.Recent())
+        }
+    }
+
+    fun insertRecentInfo(keyword: String) {
+        viewModelScope.launch {
+            getSearchInfoUseCase.insertRecentInfo(keyword).collectLatest {
+                checkedResult(
+                    dbResult = it,
+                    success = { massage -> Log.d("DB Insert Checked", massage) }
+                )
+            }
         }
     }
 }
