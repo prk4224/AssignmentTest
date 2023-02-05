f0695acc1ab36bc217e081b4b1203e66e32f7418
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-02-05 13:13:35 +0900

    feat: DB Constants 추가

---
+++ data/src/main/java/com/jaehong/data/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.jaehong.data.util
+
+object Constants {
+    const val MOVIE_JSON = "movie.json"
+    const val DB_SUCCESS_MESSAGE = "DataBase에 접근하는 것을 성공헀습니다."
+    const val DB_ERROR_MESSAGE = "DataBase에 접근하는 것을 실패헀습니다."
+}
