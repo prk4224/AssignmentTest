483247a2cbbb57881e15ec3e23ac2e022781195d
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-02-06 13:21:04 +0900

    feat: ProgressBar UI 구현

---
+++ presenter/src/main/java/com/jaehong/presentation/ui/screen/search/progress/CircularProgressBar.kt
@@ -0,0 +1,25 @@
+package com.jaehong.presentation.ui.screen.search.progress
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CircularProgressBar() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CircularProgressIndicator(
+            color = Color.Black,
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 100.dp)
+        )
+    }
+}
